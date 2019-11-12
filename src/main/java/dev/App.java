a0c37b3436cbde358d38ad1de4ada6bb29168493
Zhanna <j.santybay@gmail.com>
2019-11-12 13:48:36 +0100

    tp1 : maven : stage7

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,15 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+public class App {
+	  public static void main(String[] args) {
+	    String asciiArt = FigletFont.convertOneLine("Hello World!");
+	    System.out.println(asciiArt);
+	    String asciiArt2 = FigletFont.convertOneLine("Wonderful!");
+	    System.out.println(asciiArt2);
+	  }
+	}
