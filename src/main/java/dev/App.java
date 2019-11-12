113a2e98229d1b717470fedbd7cbe2ce7a733251
Zhanna <j.santybay@gmail.com>
2019-11-12 16:16:47 +0100

    tp3 : maven: profiles

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,15 +1,19 @@
 package dev;
 
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ResourceBundle;
+
 /**
  * Hello world!
  *
  */
 public class App {
-	  public static void main(String[] args) {
-	    String asciiArt = FigletFont.convertOneLine("Hello World!");
-	    System.out.println(asciiArt);
-	    String asciiArt2 = FigletFont.convertOneLine("Wonderful!");
-	    System.out.println(asciiArt2);
-	  }
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
 	}
+}
